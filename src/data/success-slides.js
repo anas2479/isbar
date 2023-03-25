dcc3e92317b9aaeecba52a65ed4f2dbe930fd0ea
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2023-03-25 15:38:28 -0500

    compile some Giphy GIFS
    Put together an array of Giphies to show when someone gets the right asnwere

---
+++ src/data/success-slides.js
@@ -0,0 +1,24 @@
+
+export let prompts =[
+    "Inaba Caadi Matihid",
+    "yaaaas!",
+    "Meeshaas kahay!",
+    "Mucalim baad noqon rabtaa!",
+    "laacib!",
+    "Yu aar jiniyas!",
+]
+
+export let images = [
+    `<div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/5n067EUZwH8cvtRfGz" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/126bpm-dancingatclub-5n067EUZwH8cvtRfGz">via GIPHY</a></p>`,
+
+    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/mFTIkcmQWTbNFrTp2O" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/shaymitchell-shay-mitchell-mFTIkcmQWTbNFrTp2O">via GIPHY</a></p>`,
+
+    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/mFTIkcmQWTbNFrTp2O" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/shaymitchell-shay-mitchell-mFTIkcmQWTbNFrTp2O">via GIPHY</a></p>`,
+
+    `<div style="width:100%;height:0;padding-bottom:99%;position:relative;"><iframe src="https://giphy.com/embed/l2JhL1AzTxORUTDlC" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/nba-dance-dancing-l2JhL1AzTxORUTDlC">via GIPHY</a></p>`,
+
+    `<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/3otPoK6i6fYp80YwJW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/nba-dance-dancing-3otPoK6i6fYp80YwJW">via GIPHY</a></p>`,
+
+    `<div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/htHMAjOrrDcUXW7Bn8" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/prettydudesweb-the-golden-girls-dorothy-zbornak-bea-arthur-htHMAjOrrDcUXW7Bn8">via GIPHY</a></p>`
+
+]
