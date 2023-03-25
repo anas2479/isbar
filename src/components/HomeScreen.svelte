20f94d458c39a936b6d8ac506d3279541902af1f
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2023-03-25 12:34:09 -0500

    Create the initial home screen component

---
+++ src/components/HomeScreen.svelte
@@ -0,0 +1,28 @@
+<div class="max-w-6xl mx-auto p-4 flex flex-col gap-4" id="app">
+
+    
+  
+    <div class="h- h-9"/>
+  
+    <div class="w-full" id="slide">
+      <div class="">
+        <h1 class="text-5xl font-medium">
+          Dooro Cashar
+        </h1>
+      </div>
+    
+      <div class="h- h-9"/>
+    
+      
+    </div>
+  </div>
+  
+  <script>
+      import { onMount } from "svelte";
+    import { loading } from "../store";
+      
+      onMount(() => {
+         
+        loading.set(false);
+      });
+  </script>
