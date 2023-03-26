e804e5f956ef22e834c9dd44e7950a887b861aab
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2023-03-25 22:38:40 -0500

    Set some global state variables
    Set some svelte stores to use around the app for state management.

--- src/store.js
+++ src/store.js
@@ -1,2 +1,8 @@
 import { writable } from 'svelte/store';
+
+export let currentLesson = writable(null);
+export let currentSlide = writable(null);
+
+export let screen = writable('home');
+
 export let loading = writable(true);
