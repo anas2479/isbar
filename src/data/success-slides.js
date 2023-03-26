039077992aa39550f30bca92e2caf1db994fd2f5
Anas Mohamed <49694746+anas2479@users.noreply.github.com>
2023-03-26 11:43:52 -0500

    update the GIFs to more appropriate ones

--- src/data/success-slides.js
+++ src/data/success-slides.js
@@ -9,16 +9,10 @@ export let prompts =[
 ]
 
 export let images = [
-    `<div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/5n067EUZwH8cvtRfGz" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/126bpm-dancingatclub-5n067EUZwH8cvtRfGz">via GIPHY</a></p>`,
-
-    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/mFTIkcmQWTbNFrTp2O" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/shaymitchell-shay-mitchell-mFTIkcmQWTbNFrTp2O">via GIPHY</a></p>`,
-
-    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/mFTIkcmQWTbNFrTp2O" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/shaymitchell-shay-mitchell-mFTIkcmQWTbNFrTp2O">via GIPHY</a></p>`,
-
-    `<div style="width:100%;height:0;padding-bottom:99%;position:relative;"><iframe src="https://giphy.com/embed/l2JhL1AzTxORUTDlC" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/nba-dance-dancing-l2JhL1AzTxORUTDlC">via GIPHY</a></p>`,
-
-    `<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/3otPoK6i6fYp80YwJW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/nba-dance-dancing-3otPoK6i6fYp80YwJW">via GIPHY</a></p>`,
-
-    `<div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/htHMAjOrrDcUXW7Bn8" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/prettydudesweb-the-golden-girls-dorothy-zbornak-bea-arthur-htHMAjOrrDcUXW7Bn8">via GIPHY</a></p>`
+    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/29aPEcBjA5kbP1rbtx" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`,
+    `<div style="width:100%;height:0;padding-bottom:99%;position:relative;"><iframe src="https://giphy.com/embed/ibpE2TjY6mh0LhMky1" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`,
+    `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/W5OUwJv13HVHcAKJiJ" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`,
+    `<div style="width:100%;height:0;padding-bottom:57%;position:relative;"><iframe src="https://giphy.com/embed/ll6opfxA41B6ngqPac" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`,
+    
 
 ]
